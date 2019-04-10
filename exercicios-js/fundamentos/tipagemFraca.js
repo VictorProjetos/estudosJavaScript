d9229dbeb4421c3fbf73e7f0862282b045ca1afc
Victor Fernandes <sistemas7@ls.local>
2019-04-10 10:14:28 -0300

    Ajuste e teste no github

--- exercicios-js/fundamentos/tipagemFraca.js
+++ exercicios-js/fundamentos/tipagemFraca.js
@@ -14,8 +14,8 @@ let pqp = false; // Produto Químico Perigoso...
 /* CONSIDERAÇÕES FINAIS E ANOTAÇÃO DA AULA
 
 O JavaScript é uma linguagem de "Tipagem Dinamico", ou seja, fracamente tipada, isso a torna muito
-mais flexivel que o Java por exemplo, mas isso a torna mais dificil de encontrar BUG's devidos a tipos de dados,
-onde no Java nesta questão, é muito melhor. 
+mais flexivel que o Java por exemplo, mas isso a torna mais dificil
+de encontrar BUG's devidos a tipos de dados, onde no Java nesta questão, é muito melhor. 
 
 SEMPRE evitar usar nomes genericos para variaveis, pense que alguem lerá seu código
 e com nomes claros, será bem mais facil entender o seu trabalho.
